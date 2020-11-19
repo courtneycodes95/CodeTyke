7a62b1e515a326817ede162a42df913d6da9d464
Courtney Edwards <courtney.b.edwards95@gmail.com>
2020-11-19 10:29:43 -0500

    button disabled on page load, user responses saved to state

--- src/components/learningModule/LearningModule.js
+++ src/components/learningModule/LearningModule.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SelectionBox from '../selectionBox/SelectionBox';
 import Button from '../button/Button';
 import ProgressBar from '../progressBar/ProgressBar';
@@ -13,6 +13,7 @@ const LearningModule = ({setGameStatus}) => {
   const [quizData, setQuizData] = React.useState({});
   const [showLoader, setShowLoader] = React.useState(false);
   const [showModal, setShowModal] = React.useState(false);
+  const [userResponse, setUserResponse] = React.useState([])
 
   let currentQuestion = quizData.questionArr ? quizData.questionArr[currentQuestionId]: {};
   React.useEffect(()=>{
@@ -31,22 +32,34 @@ const LearningModule = ({setGameStatus}) => {
   }
 
   const handleSubmit=()=> {
-    if(currentQuestionId < quizData.totalQuestions-1){
-      setShowLoader(true);
-      setTimeout(function(){
-        console.log("Checking answer...");
-        setCurrentQuestionId(currentQuestionId+1);
-        setShowLoader(false);
-      }, 500 );
-    } else {
-      setCurrentQuestionId(0);
-      setGameStatus({message: "Great Job! Play again.", loadIntro: true});
-    }
-  }
+    if (userResponse.length) {
+      if(currentQuestionId < quizData.totalQuestions-1){
+        setShowLoader(true);
+        setTimeout(function(){
+          console.log("Checking answer...");
+          setCurrentQuestionId(currentQuestionId+1);
+					setShowLoader(false);
+				}, 500 );
+			} else {
+				setCurrentQuestionId(0);
+				setGameStatus({message: "Great Job! Play again.", loadIntro: true});
+			}
+		}
+	}
+	
+	const handleUserChange = (selection, isChecked) => {
+		if (isChecked) {
+			setUserResponse([...userResponse, possibleAnswers[selection].props])
+		} else {
+			let updatedResponse = userResponse.filter((answer) => answer.id !== selection)
+			setUserResponse(updatedResponse);
+		}
+	}
+	
   let possibleAnswers = [];
   if(currentQuestion.possibleAnswers){
     possibleAnswers = currentQuestion.possibleAnswers.map((answer, index) => {
-      return <SelectionBox id={index} key={index} answer={answer} />
+      return <SelectionBox id={index} key={index} answer={answer} handleUserChange={handleUserChange} />
     })
   }
 
@@ -80,7 +93,14 @@ const LearningModule = ({setGameStatus}) => {
               { possibleAnswers }
             </div>
             <div className="learningModule--submitButtonContainer">
-              <Button label="Submit" handleSubmit={ handleSubmit } showLoader={showLoader} hasIcons />
+              <Button 
+								label="Submit"
+								handleSubmit={handleSubmit}
+								showLoader={showLoader}
+								hasIcons
+								disabled={!userResponse.length}
+								showArrow={userResponse.length ? true : false}
+							/>
             </div>
           </div>
         </>
